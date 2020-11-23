e46017c83bde572666744b2844b5b87400621c7d
Boyle <TrBoyle@asrcfederal.com>
2020-11-23 12:55:17 -0900

    Moved code into repo

---
+++ Converter.cs
@@ -0,0 +1,165 @@
+using System;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Collections.Generic;
+using System.CodeDom;
+using System.CodeDom.Compiler;
+using System.Reflection;
+
+namespace EFCoreTsGen
+{
+    public static class Extensions
+    {
+
+        public static ClassAndLines GetClassAndLines(this string csFilePath)
+        {
+            List<string> allLines = File.ReadAllLines(csFilePath).ToList<string>();
+
+            int classNameIndex = allLines.FindIndex(l => l.Contains(" class "));
+
+            string className = allLines.Where(l => l.Contains(" class ")).Select(l => l.Substring(l.LastIndexOf(' ') + 1)).Single();
+
+            //Get lines inside class
+            List<string> filteredLines = allLines.GetRange(classNameIndex + 2, allLines.Count() - (classNameIndex + 4));
+            filteredLines = filteredLines.Where(l => l.Length > 0).ToList(); //Remove blank lines
+
+
+            return new ClassAndLines() { ClassName = className, Lines = filteredLines };
+        }
+
+        public static string BuildTsStr(this ClassAndLines classAndLines, List<string> allClassNames)
+        {
+            string tsClassStr = string.Empty;
+
+            tsClassStr += "export class " + classAndLines.ClassName + " {\r\n";
+
+            List<string> modifiedLines = new List<string>();
+            //Remove get and set
+            //Remove public
+            //Remove virtual
+            foreach (string l in classAndLines.Lines)
+            {
+                string lineToAdd = l;
+                List<string> removeMeList = new List<string>() { " { get; set; }", "virtual", "public" };
+                foreach (string removeMe in removeMeList)
+                {
+                    lineToAdd = lineToAdd.RemoveThis(removeMe);
+                }
+                modifiedLines.Add(lineToAdd.Trim());
+            }
+
+            //Remove constructor if exists
+            int endOfConstructorIndex = modifiedLines.FindLastIndex(l => l.Contains("}"));
+            if (endOfConstructorIndex != -1)
+            {
+                modifiedLines = modifiedLines.GetRange(endOfConstructorIndex + 1, modifiedLines.Count() - (endOfConstructorIndex + 1));
+            }
+
+            List<string> importList = new List<string>();
+            foreach (string line in modifiedLines)
+            {
+                string typeStr = line.Substring(0, line.IndexOf(" "));
+
+                string propName = line.Substring(line.LastIndexOf(' ') + 1);
+
+                //Convert to camelcase
+                var cc = new CamelCasePropertyNamesContractResolver();
+                propName = cc.GetResolvedPropertyName(propName);
+
+                tsClassStr += $"   public {propName}: {typeStr.TypeConversion(allClassNames, importList)};\r\n";
+            }
+
+            tsClassStr += "}";
+
+            //add imports
+            string importStr = "\r\n";
+            //Converting to hashset removes duplicates
+            foreach (string import in importList.ToHashSet<string>())
+            {
+                //don't import self
+                if (!(classAndLines.ClassName==import))
+                {
+                    importStr += "import { " + import + " } from './index';\r\n";
+                }
+            }
+
+            tsClassStr = importStr + "\r\n" + tsClassStr;
+
+            return tsClassStr;
+        }
+
+        private static string TypeConversion(this string csTypeStr, List<string> allClassNames, List<string> importList)
+        {
+            //Default to number since lots of C# stuff rolls up to this in ts
+            string tsTypeStr = string.Empty;
+
+            switch (csTypeStr)
+            {
+                case "bool":
+                case "boolean":
+                    tsTypeStr = "boolean = false";
+                    break;
+                case "boolean?":
+                case "bool?":
+                    tsTypeStr = "boolean = null";
+                    break;
+                case "string":
+                    tsTypeStr = "string = null";
+                    break;
+                case "DateTime":
+                    tsTypeStr = "Date = new Date(0)";
+                    break;
+                case "DateTime?":
+                    tsTypeStr = "Date = null";
+                    break;
+                case "int?":
+                case "decimal?":
+                    tsTypeStr = "number = null";
+                    break;
+                case "byte[]":
+                    tsTypeStr = "Array<any> = []";
+                    break;
+                case "Guid":
+                    tsTypeStr = "string = '00000000-0000-0000-0000-000000000000'";
+                    break;
+                default:
+                    tsTypeStr = "number = 0";
+                    if (csTypeStr.Contains("ICollection"))
+                    {
+                        int startIndex = csTypeStr.IndexOf('<')+1;
+                        int lengthToGet = csTypeStr.IndexOf('>') - startIndex;
+                        tsTypeStr = csTypeStr.Substring(startIndex,lengthToGet);
+                        importList.Add(tsTypeStr);
+                        tsTypeStr = tsTypeStr + "[] = []";
+                    }
+                    else if (allClassNames.Contains(csTypeStr))
+                    {
+                        importList.Add(csTypeStr);
+                        tsTypeStr = csTypeStr + " = null";
+                    }
+                    break;
+            }
+
+            return tsTypeStr;
+        }
+
+        private static string RemoveThis(this string fullStr, string removeMe)
+        {
+            int index = fullStr.IndexOf(removeMe);
+            string retStr = (index < 0) ? fullStr : fullStr.Remove(index, removeMe.Length);
+            return retStr;
+        }
+    }
+
+}
+
+
+public class ClassAndLines
+{
+    public string ClassName { get; set; }
+
+    public List<string> Lines { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Collections.Generic;
+using System.CodeDom;
+using System.CodeDom.Compiler;
+using System.Reflection;
+
+namespace EFCoreTsGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string SETTINGS_FILE_NAME = "efcoretsgen.settings.json";
+            string settingsPath = args.FirstOrDefault() ?? ".\\";
+            string settingsFilePath = $"{settingsPath}\\{SETTINGS_FILE_NAME}";
+
+            Console.WriteLine($"Looking for {SETTINGS_FILE_NAME} at {settingsPath}...");
+
+            if (File.Exists(settingsFilePath)){
+                Console.WriteLine("Found settings file...");
+
+                JObject settingsObj = JObject.Parse(File.ReadAllText(settingsFilePath));
+
+                string modelsPath = Path.GetFullPath(settingsPath + settingsObj["modelPath"].Value<string>());
+                List<string> excludeList= settingsObj["excludeStrings"].ToObject<List<string>>();
+
+                Console.WriteLine($"Reading EF Models from {modelsPath}, excluding '{String.Join("','",excludeList)}'");
+
+                var modelFileNames = Directory.EnumerateFiles(modelsPath, "*.cs").Where(m => !excludeList.Any(e => m.Contains(e)));
+                
+                //Parse out the class name and the lines internal to the class
+                List<ClassAndLines> classAndLines = new List<ClassAndLines>();
+                foreach (string csFilePath in modelFileNames)
+                {
+                    classAndLines.Add(csFilePath.GetClassAndLines());
+                }
+
+                foreach (ClassAndLines cl in classAndLines)
+                {
+                    //Write ts files into specified dir
+                    string outputDir = Path.GetFullPath(settingsObj["outputPath"].Value<string>());
+                    string filePath = outputDir + "\\" + cl.ClassName + ".ts";
+                    File.WriteAllText(filePath, cl.BuildTsStr(classAndLines.Select(cl => cl.ClassName).ToList()));
+
+                }
+
+                
+                Console.WriteLine($"Complete.  {classAndLines.Count()} files created or updated!");
+
+            }
+            else {
+                Console.WriteLine("No settings file found, exiting!");
+            }
+
+        }
+    }
+}
